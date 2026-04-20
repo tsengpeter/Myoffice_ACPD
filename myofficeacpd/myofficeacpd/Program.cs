87b956b705d3928ccd2b3781211a107580d547a6
peterzen4 <peterzen4@gmail.com>
2026-04-20 14:47:11 +0800

    feat: 加入 Swagger UI 並完成 Docker 容器化設定
    - 安裝 Swashbuckle.AspNetCore，Swagger UI 掛載於根路徑（/）
    - 新增 appsettings.Docker.json，容器內透過 host.docker.internal 連接 SQL Server
    - Dockerfile 加入 ASPNETCORE_ENVIRONMENT=Docker 環境變數
    - 新增 docker-compose.yml 方便一鍵啟動

--- myofficeacpd/myofficeacpd/Program.cs
+++ myofficeacpd/myofficeacpd/Program.cs
@@ -11,31 +11,31 @@ namespace myofficeacpd
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddDbContext<MyofficeAcpdDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("Myoffice_ACPD")));
 
             builder.Services.AddScoped<IMyofficeacpdService, MyofficeacpdService>();
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "Myoffice ACPD API", Version = "v1" });
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.MapOpenApi();
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Myoffice ACPD API v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
