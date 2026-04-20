b2a255dbac8d3e73551b8b3f8657d2bc487bd284
peterzen4 <peterzen4@gmail.com>
2026-04-20 13:59:46 +0800

    feat: 新增 MyofficeacpdController
    - 新增 MyofficeacpdController 作為主要 API 入口
    - 移除預設 WeatherForecast Controller 與 Model
    - 新增 .gitignore 排除 bin/、obj/、.vs/ 建置產出物

---
+++ myofficeacpd/myofficeacpd/Controllers/MyofficeacpdController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myofficeacpd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyofficeacpdController : ControllerBase
+    {
+    }
+}
